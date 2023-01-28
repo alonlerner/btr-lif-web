5443c7c22523e75301f4bfab69d11aa4587a3f5c
alonlerner <lernera97@gmail.com>
2023-01-28 16:40:24 -0500

    improved navbar and implemented access

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import Navbar from './Navbar'
-import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function App() {
+  const [access, setAccess] = React.useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar/>
-      </header>
+      {!access && <p onClick={() => setAccess(true)}>404 error: page not found.</p>}
+      {access && <header className="App-header">
+         <Navbar/>
+      </header>}
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import { Box, Center, Image } from '@chakra-ui/react'
 function Navbar() {
   return (
     <div className="Navbar">
-        <Box bg='#474a49' w='100%' alignItems='center' p={2} color='white'>
+        <Box bg='#3b3b3b' w='100%' alignItems='center' p={2} color='white' borderBottom='1px' borderBottomColor='#02e020'>
             <Center>
                 <Image src={logo} alt='btr lif logo' boxSize='67px'/>
             </Center>
